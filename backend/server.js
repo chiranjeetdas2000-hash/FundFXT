38bd2168a6ee5bc04d630f8f87222f693c105df7
chiranjeetdas2000-hash <chiranjeetdas2000@gmail.com>
2026-09-22 13:15:23 +0530

    Fix rate limiter proxy trust

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const { createRateLimiter, userRateLimiter } = require("./rate-limits");
 require("dotenv").config();
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cors());
 
